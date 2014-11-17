fd0946973ebc62b6ae3328e33ac6cdc79d614769
ADHEPEAU <ladhepeau@cergy.itin.fr>
2014-11-17 02:56:23 +0100

    Test Commit N°4 + Le build à péter .. Pourquoi ? Je ne sais pas !

--- src/main/java/TestJunitHelloWorld.java
+++ src/main/java/TestJunitHelloWorld.java
@@ -1,6 +1,6 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates dgg
+ * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 import static junit.framework.Assert.assertEquals;
@@ -19,6 +19,6 @@ public class TestJunitHelloWorld {
         }
     }
     public String sayHello() {
-        return "Hello Test de verifier si le build se lance en automatique";
+        return "toto";
     }
 }
